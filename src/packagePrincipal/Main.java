cca203c84b497822b95f3f523fce98d815ad31a8
luisGeovany <glopezvan@gmail.com>
2022-04-25 21:43:51 -0500

    Ejercicio tema 3

--- src/packagePrincipal/Main.java
+++ src/packagePrincipal/Main.java
@@ -1,2 +1,26 @@
-package packagePrincipal;public class Main {
+package packagePrincipal;
+
+public class Main {
+    public static void main(String[] args) {
+        // Llamar la funcion suma
+        System.out.println(suma(1,2,3));
+
+        // creamos el objeto miCoche de la clase Coche
+        Coche miCoche = new Coche();
+        miCoche.incrementarPuertas();
+        System.out.println(miCoche.puertas);
+    }
+
+    // funcion con tres parametros que sean numeros y se sumen entre si
+    public static int suma(int n1, int n2, int n3){
+        return n1 + n2 + n3;
+    }
+
+}
+class Coche {
+    public int puertas = 2;
+
+    public void incrementarPuertas(){
+        this.puertas++;
+    }
 }
